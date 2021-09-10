59a2876050093fb1715c8f92c9115d1e5c9bf733
Hanan <63251521+HananPilakkadan@users.noreply.github.com>
2021-09-10 23:37:03 +0530

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,67 +1,66 @@
-$(document).ready(function(){
-    var wow = new WOW({
-        boxClass: "wow", // animated element css class (default is wow)
-        animateClass: "animated", // animation css class (default is animated)
-        offset: 0, // distance to the element when triggering the animation (default is 0)
-        mobile: true, // trigger animations on mobile devices (default is true)
-        live: true, // act on asynchronously loaded content (default is true)
-        callback: function (box) {},
-        scrollContainer: null, // optional scroll container selector, otherwise use window,
-        resetAnimation: true, // reset animation on end (default is true)
-      });
-      wow.init();
+$(document).ready(function () {
+  var wow = new WOW({
+    boxClass: "wow", // animated element css class (default is wow)
+    animateClass: "animated", // animation css class (default is animated)
+    offset: 0, // distance to the element when triggering the animation (default is 0)
+    mobile: true, // trigger animations on mobile devices (default is true)
+    live: true, // act on asynchronously loaded content (default is true)
+    callback: function (box) {},
+    scrollContainer: null, // optional scroll container selector, otherwise use window,
+    resetAnimation: true, // reset animation on end (default is true)
+  });
+  wow.init();
 
+  $(".owl-carousel").owlCarousel({
+    loop: true,
+    nav: true,
+    dots: false,
+    margin: 10,
+    autoplay: true,
+    autoplaySpeed: 1000,
+    autoplayTimeout: 3000,
+    touchDrag: false,
+    mouseDrag: false,
+    responsive: {
+      0: {
+        items: 1,
+        nav: true,
+      },
+      600: {
+        items: 2,
+        nav: false,
+      },
+      1000: {
+        items: 3,
+        nav: true,
+        loop: true,
+      },
+    },
+  });
 
-    $('.owl-carousel').owlCarousel({
-        loop:true,
-        nav:true,
-        dots:false,
-        margin:10,
-        autoplay:true,
-        autoplaySpeed:1000,
-        autoplayTimeout:3000,
-        touchDrag:false,
-        mouseDrag:false,
-        responsive:{
-          0:{
-              items:1,
-              nav:true
-          },
-          600:{
-              items:2,
-              nav:false
-          },
-          1000:{
-              items:3,
-              nav:true,
-              loop:true,
-              
-          }
-      }
-    });
-    // image-slider-inited
-    window.onscroll = function () {
-        headerFunction();
-      };
+  // image-slider-inited
+  window.onscroll = function () {
+    headerFunction();
+  };
 
-      // var header = document.getElementById("header");
-      var body = document.body;
-      var sticky = body.offsetTop;
+  // var header = document.getElementById("header");
+  var body = document.body;
+  var sticky = body.offsetTop;
 
-      function headerFunction() {
-        if (window.pageYOffset > 150) {
-          body.classList.add("sticky");
-        } else {
-          body.classList.remove("sticky");
-        }
-      }
-})
+  function headerFunction() {
+    if (window.pageYOffset > 150) {
+      body.classList.add("sticky");
+    } else {
+      body.classList.remove("sticky");
+    }
+  }
+});
 
 // hamburger-menu
 function showMenu() {
-    $("body").toggleClass("active");
+  $("body").toggleClass("active");
 }
-  
+
 function hideMenu() {
-    $("body").removeClass("active");
+  $("body").removeClass("active");
 }
